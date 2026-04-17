7b42971d8a82b21f2c73e49b0253c93aa1f626b1
yang.xia <yang.xia@carthunder.com>
2026-04-17 14:30:37 +0800

    collect list add mock data

--- service.go
+++ service.go
@@ -534,25 +534,321 @@ func (s *XiaohongshuService) UnfavoriteFeed(ctx context.Context, feedID, xsecTok
 
 // ListCollectedNotes 获取当前登录用户的收藏列表
 func (s *XiaohongshuService) ListCollectedNotes(ctx context.Context) (*CollectedNotesResponse, error) {
-	b := newBrowser()
-	defer b.Close()
-
-	page := b.NewPage()
-	defer page.Close()
-
-	action := xiaohongshu.NewCollectAction(page)
-
-	result, err := action.GetCollectedNotes(ctx)
-	if err != nil {
+	//b := newBrowser()
+	//defer b.Close()
+	//
+	//page := b.NewPage()
+	//defer page.Close()
+	//
+	//action := xiaohongshu.NewCollectAction(page)
+	//
+	//result, err := action.GetCollectedNotes(ctx)
+	//if err != nil {
+	//	return nil, err
+	//}
+	mockData := `
+{
+  "notes": [
+    {
+      "note_id": "67caff7f00000000060283c5",
+      "display_title": "京味儿Citywalk｜一日游经典路线‼️",
+      "type": "normal",
+      "xsec_token": "ABE8_z7b22vL4whZ1uB48DMCaVGtXlvTqvngt4h9HniBU=",
+      "cover": {
+        "width": 1280,
+        "height": 1706,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/be68e675ced7e14a37b36148e0fa92dc/spectrum/1040g34o31entjhup5o3048t3r47t6815470o5to!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/da94a152c30794c53d1ec6c5598a6e3f/spectrum/1040g34o31entjhup5o3048t3r47t6815470o5to!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/be68e675ced7e14a37b36148e0fa92dc/spectrum/1040g34o31entjhup5o3048t3r47t6815470o5to!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/da94a152c30794c53d1ec6c5598a6e3f/spectrum/1040g34o31entjhup5o3048t3r47t6815470o5to!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "58384fd35e87e77148f82025",
+        "nickname": "💖旅游小堆唲💖",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/66b5a94aabe1e6168f27b52f.jpg?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABFobFcO1pdYyGmd3DTKIymi2LE3Q1yENdy8jSLiKJDPw="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "246"
+      }
+    },
+    {
+      "note_id": "67cfa22200000000280355a6",
+      "display_title": "北京24小时转机✈️一日游攻略｜特种兵来咯",
+      "type": "normal",
+      "xsec_token": "ABgKWQ5n59s6hDV7Dwm8eFKL_tnsQ6kEjVWg0iuZWIHus=",
+      "cover": {
+        "width": 1440,
+        "height": 1920,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/93114ebcb6c83434e0ea64a5d28c36bb/notes_pre_post/1040g3k031esejp0cm61g5o8rhh8gbqpf1a74lt8!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/ade05ca5998b1325dd465de172470cec/notes_pre_post/1040g3k031esejp0cm61g5o8rhh8gbqpf1a74lt8!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/93114ebcb6c83434e0ea64a5d28c36bb/notes_pre_post/1040g3k031esejp0cm61g5o8rhh8gbqpf1a74lt8!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/ade05ca5998b1325dd465de172470cec/notes_pre_post/1040g3k031esejp0cm61g5o8rhh8gbqpf1a74lt8!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "611b8c51000000000101eb2f",
+        "nickname": "比巴小卜卜",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/61507c078f474cf115b253b6.jpg?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABMpZaRKwN_H5AxqnZM7OzZYUFtsTCrjXFibPt1vhKgPY="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "52"
+      }
+    },
+    {
+      "note_id": "68c0deab000000001d02c554",
+      "display_title": "不早起版｜一日游青城山前山+都江堰蓝眼泪",
+      "type": "normal",
+      "xsec_token": "ABmHK-DIf2trebqtrZZ2IV_PyNjvGsF3tdBO8OvVH_9gE=",
+      "cover": {
+        "width": 3442,
+        "height": 4590,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/dda65ceb36076b8646807a2501c43f1c/1040g00831m810io5l8d05nuo3vt09fqdttiucdo!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/ecc05511482ddbefddb118452bbf1f50/1040g00831m810io5l8d05nuo3vt09fqdttiucdo!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/dda65ceb36076b8646807a2501c43f1c/1040g00831m810io5l8d05nuo3vt09fqdttiucdo!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/ecc05511482ddbefddb118452bbf1f50/1040g00831m810io5l8d05nuo3vt09fqdttiucdo!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "5fd81ffa000000000100bf4d",
+        "nickname": "小舟行记（流浪版🌍）",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo31frf6db1gg6g5nuo3vt09fqdb85jc50?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABjnIsAWcHDPZUEQ34z94DuSj8w5gkj-rPn1FStAm7n7g="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "1412"
+      }
+    },
+    {
+      "note_id": "6959d946000000001f004112",
+      "display_title": "成都一日游，保姆级攻略！感受慢生活～",
+      "type": "normal",
+      "xsec_token": "AB5fROYVEzTE6EIeJs63-OQaARdAN0eSPymeo35Tbkgpk=",
+      "cover": {
+        "width": 1280,
+        "height": 1707,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/8205015122b16c2d93520875b94c267a/notes_pre_post/1040g3k031qtdua8bg0005ol99uimspoc166d1o8!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/a2f3eb7d03982cb8b819c38dbf828b59/notes_pre_post/1040g3k031qtdua8bg0005ol99uimspoc166d1o8!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/8205015122b16c2d93520875b94c267a/notes_pre_post/1040g3k031qtdua8bg0005ol99uimspoc166d1o8!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/a2f3eb7d03982cb8b819c38dbf828b59/notes_pre_post/1040g3k031qtdua8bg0005ol99uimspoc166d1o8!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "62a94fa5000000001b02670c",
+        "nickname": "铁板豆腐",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo317ejjkoc44005ol99uimspoc8v3m10g?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABSoxi9E1yIny8rvJvJ_O9j-UitF5xa3rJfrJOtGJnVuU="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "373"
+      }
+    },
+    {
+      "note_id": "69dcbeb500000000220000e7",
+      "display_title": "北京五一，经典必逛Citywalk路线，就这❺条❗️",
+      "type": "normal",
+      "xsec_token": "ABzT8M66aOIv-zx2QHmlSe6dw2BnI6XITHcK_F1UT-7fU=",
+      "cover": {
+        "width": 1920,
+        "height": 2560,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/b28651d3915a45d9786349d67ebdbf5e/notes_pre_post/1040g3k031utcj14r2q005oaikapgkc6p8qmfrv8!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/5a316ef7c09d952919741e5662af1d22/notes_pre_post/1040g3k031utcj14r2q005oaikapgkc6p8qmfrv8!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/b28651d3915a45d9786349d67ebdbf5e/notes_pre_post/1040g3k031utcj14r2q005oaikapgkc6p8qmfrv8!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/5a316ef7c09d952919741e5662af1d22/notes_pre_post/1040g3k031utcj14r2q005oaikapgkc6p8qmfrv8!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "6152a2b300000000020230d9",
+        "nickname": "阿七在北京",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo311981gromc005oaikapgkc6pba7lre0?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABFr2KFf4fvNDNausmFlRbOxRafB5PdMqtfVtD_x7Fb44="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "148"
+      }
+    },
+    {
+      "note_id": "69c7a30e000000001f000511",
+      "display_title": "北京四天人均2K｜保姆级逛吃玩攻略＋附路线",
+      "type": "normal",
+      "xsec_token": "ABsKEQkWdG7U7N635mpKVUg8HDEQnwVWJHcBDtOjtQSWE=",
+      "cover": {
+        "width": 1920,
+        "height": 2560,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/736cadf6458880ff7d3fc435327d49e8/1040g2sg31u8is1toia3g45bbl27pds4t63ns08o!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/e6bc8e999a71976482d74a514f52f707/1040g2sg31u8is1toia3g45bbl27pds4t63ns08o!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/736cadf6458880ff7d3fc435327d49e8/1040g2sg31u8is1toia3g45bbl27pds4t63ns08o!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/e6bc8e999a71976482d74a514f52f707/1040g2sg31u8is1toia3g45bbl27pds4t63ns08o!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "56a48f965e87e726d289f09d",
+        "nickname": "大欢干啥呢",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo31p3cli3njc6045bbl27pds4trq8gdl0?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABOz2X0is4tcNHXuN84Se8bF-nx5NF63HQUxdzs0X2jj0="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "538"
+      }
+    },
+    {
+      "note_id": "69c0e2d4000000002102c0d2",
+      "display_title": "北京🍂｜5天4晚详细旅行攻略（图文版）",
+      "type": "normal",
+      "xsec_token": "ABWAjhoYZTjBObFGjisc8or6EPoftzKj2b_JsbACXdaGI=",
+      "cover": {
+        "width": 3072,
+        "height": 4096,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/0c34d598137c91cf7b9c21bdc7c04a7e/notes_pre_post/1040g3k831u23gt9p7e704bo8cvraam60pdi7u68!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/f0de0ccf62c21bdd03af561bca8564ac/notes_pre_post/1040g3k831u23gt9p7e704bo8cvraam60pdi7u68!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/0c34d598137c91cf7b9c21bdc7c04a7e/notes_pre_post/1040g3k831u23gt9p7e704bo8cvraam60pdi7u68!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/f0de0ccf62c21bdd03af561bca8564ac/notes_pre_post/1040g3k831u23gt9p7e704bo8cvraam60pdi7u68!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "5be076a5d784f10001c758c0",
+        "nickname": "辣鸡脆堡yeah",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/67616f0b59d26cd13a27fa6b.jpg?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABdIJUmuhyYqqfUNGN27ZGJVB1T8zOycLU1GChDxaYqsk="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "1359"
+      }
+    },
+    {
+      "note_id": "6800ea53000000001e005f71",
+      "display_title": "拒绝人挤人！📍北京五一❾个小众浪漫好去处🍃",
+      "type": "normal",
+      "xsec_token": "ABPPx5lfoFqJe9UxmuyL_x0KlAlJvBXx25fBEkcDAzHbg=",
+      "cover": {
+        "width": 1920,
+        "height": 2560,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/353eb503a417701ced428f76343299cb/1040g00831gcipekrji0049mi5onng189a13h28g!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/eb70e8058ab457e3ca8484e40039b4d7/1040g00831gcipekrji0049mi5onng189a13h28g!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/353eb503a417701ced428f76343299cb/1040g00831gcipekrji0049mi5onng189a13h28g!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/eb70e8058ab457e3ca8484e40039b4d7/1040g00831gcipekrji0049mi5onng189a13h28g!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "5972af786a6a6901e8ef0509",
+        "nickname": "想吃披萨汉堡和好多肉",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo30og0o2v1240049mi5onng189npnvph0?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABMmcWWvC5j_ODHCPhaFfMMbEAP-tNxq7cu5nd9XZujTw="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "1255"
+      }
+    },
+    {
+      "note_id": "69ddf739000000001a031d31",
+      "display_title": "北京近郊| 这和“赛里木湖”没差吧？",
+      "type": "normal",
+      "xsec_token": "ABmN0jY5uDjU-xqpD_qVvc7ydO38Ksryf6nR9udkBTAtM=",
+      "cover": {
+        "width": 1536,
+        "height": 2048,
+        "url_pre": "http://sns-webpic-qc.xhscdn.com/202604171404/bca7005938e9c7ffe72f645ca55d5be7/1040g2sg31uueksguiqe049ul1fn8p3eia2etbv8!nc_n_webp_prv_1",
+        "url_default": "http://sns-webpic-qc.xhscdn.com/202604171404/f3d13fba4c322a7af6cd3e8f6fbd79f7/1040g2sg31uueksguiqe049ul1fn8p3eia2etbv8!nc_n_webp_mw_1",
+        "info_list": [
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/bca7005938e9c7ffe72f645ca55d5be7/1040g2sg31uueksguiqe049ul1fn8p3eia2etbv8!nc_n_webp_prv_1"
+          },
+          {
+            "imageScene": "",
+            "url": "http://sns-webpic-qc.xhscdn.com/202604171404/f3d13fba4c322a7af6cd3e8f6fbd79f7/1040g2sg31uueksguiqe049ul1fn8p3eia2etbv8!nc_n_webp_mw_1"
+          }
+        ]
+      },
+      "user": {
+        "user_id": "59e72e8c4eacab247a148dd2",
+        "nickname": "就得做大哥",
+        "avatar": "https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo31rsv7ssg320g49ul1fn8p3eiedqb8g0?imageView2/2/w/120/format/jpg",
+        "xsec_token": "ABc24JSDJkq6rF4jzjtCngLJ0pCCZyHM1kzSpn-Pfgq7s="
+      },
+      "interact_info": {
+        "liked": false,
+        "liked_count": "1857"
+      }
+    }
+  ],
+  "has_more": false,
+  "cursor": "69ddf739000000001a031d31",
+  "count": 9
+}
+`
+	var response CollectedNotesResponse
+	if err := json.Unmarshal([]byte(mockData), &response); err != nil {
+		logrus.Warnf("unmarshal mock collect data error: %v", err)
 		return nil, err
 	}
-
-	return &CollectedNotesResponse{
-		Notes:   result.Notes,
-		HasMore: result.HasMore,
-		Cursor:  result.Cursor,
-		Count:   len(result.Notes),
-	}, nil
+	return &response, nil
 }
 
 // ReplyCommentToFeed 回复指定评论
